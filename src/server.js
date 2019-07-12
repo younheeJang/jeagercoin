df6a0c46d487420704ad624a2079d174d90a2e98
apple <apple@appleui-iMac.local>
2019-07-12 15:01:21 +0900

    download rest client, and make some api with request.http file

--- src/server.js
+++ src/server.js
@@ -11,4 +11,15 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(morgan("combined"));
+
+app.get("/blocks", (req, res) => {
+    res.send(getBlockchain());
+})
+
+app.post("/blocks", (req, res) => {
+    const { body : { data } } = req;
+    const newBlock = createNewBlock(data);
+    res.send(newBlock);
+})
+
 app.listen(PORT, () => console.log(`jeagercoin server running on ${PORT}`));

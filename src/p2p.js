24af85e68af375e9e225445c720acd60271dc82a
apple <apple@appleui-iMac.local>
2019-07-12 16:22:27 +0900

    execute various port example 3000, 4000

--- src/p2p.js
+++ src/p2p.js
@@ -2,14 +2,28 @@ const WebSockets = require("ws");
 
 const sockets = [];
 
+const getSockets = () => sockets;
+
 const startP2PServer = server => {
     const wsServer = new WebSockets.Server({ server });
     wsServer.on("connection", ws => {
-        console.log(`hello ${ws}`);
+        console.log(`hello ${{ws}}`);
     });
     console.log("jeagercoin p2p server running ");
 };
 
+const initSocketConnection = socket => {
+    sockets.push(socket);
+}
+
+const connectToPeers = newPeer => {
+    const ws = new WebSockets(newPeer);
+    ws.on("open", () => {
+        initSocketConnection(ws);
+    })
+}
+
 module.exports = {
-  startP2PServer   
+  startP2PServer,
+  connectToPeers   
 }
--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const express = require("express"),
 
 
 const { getBlockchain, createNewBlock } = Blockchain;
-const { startP2PServer } = P2P;
+const { startP2PServer, connectToPeers } = P2P;
 
 const PORT = process.env.HTTP_PORT || 3000;
 
@@ -24,6 +24,12 @@ app.post("/blocks", (req, res) => {
     const newBlock = createNewBlock(data);
     res.send(newBlock);
 })
+
+app.post("/peers", (req, res) => {
+    const { body : { peer } } = req;
+    connectToPeers(peer);
+    res.send();
+});
 
 const server = app.listen(PORT, () => console.log(`jeagercoin HTTP server running on ${PORT}`));
 

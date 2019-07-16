514173784afee64e56f4024e49d68090ea06dedb
apple <apple@appleui-iMac.local>
2019-07-16 20:07:56 +0900

    update function replaceChain replace if statement  not length, sumDifficulty

--- src/blockchain.js
+++ src/blockchain.js
@@ -180,10 +180,16 @@ const isChainValid = candidateChain => {
   return true;
 };
 
+const sumDifficulty = anyBlockchain => 
+    anyBlockchain
+        .map(block => block.difficulty)
+        .map(difficulty => Math.pow(2,difficulty))
+        .reduce((a,b) => a + b);
+
 const replaceChain = candidateChain => {
   if (
     isChainValid(candidateChain) &&
-    candidateChain.length > getBlockchain().length
+    sumDifficulty(candidateChain)
   ) {
     blockchain = candidateChain;
     return true;

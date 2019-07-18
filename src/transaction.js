0bd58f2bbf6d49ca4e1cb28b6868a0df758a48c2
apple <apple@appleui-iMac.local>
2019-07-18 21:21:21 +0900

    add elliptic and make function called findTUxOUt for using input

--- src/transaction.js
+++ src/transaction.js
@@ -1,4 +1,7 @@
-const CryptoJS = require("crypto-js");
+const CryptoJS = require("crypto-js"),
+    elliptic = require("elliptic");
+
+const ec = new EC("secp256k1");
 
 class TxOut{
     constructor(address, amount){
@@ -20,9 +23,9 @@ class Transaction {
 }
 
 class UTxOut {
-    constructor(uTxOutId, uTxOutIndex, address, amount){
-        this.uTxOutId = uTxOutId;
-        this.uTxOutIndex = uTxOutIndex;
+    constructor(txOutId, txOutIndex, address, amount){
+        this.txOutId = txOutId;
+        this.txOutIndex = txOutIndex;
         this.address = address;
         this.amount = amount;
     }
@@ -43,5 +46,26 @@ const getTxId = tx => {
     return CryptoJS.SHA256(txInContent + txOutContent).toString();
 };
 
+//when you want to use input that should be previous outputs so
+//you find the output first and use it for using input.
+const findUTxOut = (txOutId, txOutIndex, uTxOutList) => {
+    return uTxOutList.find(
+        uTxOut => uTxOut.txOutId === txOutId &&
+        uTxOut.txOutIndex === txOutIndex
+        );
+}
+
+
+//sigining transaction input
+//+using library called crypto.js and elliptic
+const signTxIn = (tx, txInIndex, privateKey, uTxOut) => {
+    const txIn = tx.txIns[txInIndex];
+    const dataToSign = tx.id;
+
+    const referencedUTxOut = findUTxOut(txIn.txOutId, txIn.txOutIndex, uTxOuts);
+    if(referencedUTxOut === null){
+        return;
+    }
+};
 
 

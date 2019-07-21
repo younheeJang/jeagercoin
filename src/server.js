88eb97f8952587900d580902ec6d08c32c728459
apple <apple@appleui-iMac.local>
2019-07-22 00:45:08 +0900

    what worked: generate privatekey, initiate wallet

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const express = require("express"),
     bodyParser = require("body-parser"),
     morgan = require("morgan"),
     Blockchain = require("./blockchain"),
-    P2P = require("./p2p");
+    P2P = require("./p2p"),
+    Wallet = require("./wallet");
 
 
 const { getBlockchain, createNewBlock } = Blockchain;
 const { startP2PServer, connectToPeers } = P2P;
+const { initWallet } = Wallet;
 
 const PORT = process.env.HTTP_PORT || 3000;
 
@@ -33,4 +35,5 @@ app.post("/peers", (req, res) => {
 
 const server = app.listen(PORT, () => console.log(`jeagercoin HTTP server running on ${PORT}`));
 
+initWallet();
 startP2PServer(server);

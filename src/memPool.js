6a04f3107cba4eaf4033f2c8bb4e41bb1756f1e5
apple <apple@appleui-iMac.local>
2019-07-26 02:43:46 +0900

    validate input for mempool

--- src/memPool.js
+++ src/memPool.js
@@ -4,3 +4,30 @@ const _ = require("lodash"),
 const { validateTx } = Transactions;
 
 let memPool = [];
+
+const getTxInsInPool = memPool => {
+    return _(memPool)
+        .map(tx => tx.txIns)
+        .flatten()
+        .value();
+};
+
+const isTxValidForPool = (tx, memPool) => {
+    const txInsInPool = getTxInsInPool(memPool);
+
+    const isTxInAllreadyInPool = (txIns, txIn) => {
+        return _.find(txIns, txInInPool => {
+            return (
+                txIn.txOutIndex === txInInPool.txOutIndex &&
+                txIn.txOutId === txInInPool.txOutId
+            );
+        });
+    };
+
+    for(const txIn of tx.txIns){
+        if(isTxInAllreadyInPool(txInsInPool, txIn)){
+            return false;
+        }
+    }
+    return true;
+}
